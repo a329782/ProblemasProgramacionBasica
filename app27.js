0edc19dd60b4ca977ce922ce4a204efc00753e64
a329782 <a329782@uach.mx>
2021-04-27 17:47:03 -0600

    27.- Realizar la tabla de multiplicar de un numero entre 0 y 10.

---
+++ app27.js
@@ -0,0 +1,20 @@
+//27.- Realizar la tabla de multiplicar de un numero entre 0 y 10.
+
+var num;
+
+process.stdout.write("Introduce un numero (mayor que 0 y menor que 10): ");
+process.stdin.on('data', function(data){
+    num = data;
+    if(num > 0 && num < 11)
+    {
+        for(var i = 1; i < 11; i++)
+        {
+            console.log(num + " x " + i + " = " + num*i);
+        }
+    }
+    else
+    {
+        console.log("El numero no es valido compadRe.");
+    }
+    process.exit();
+});
