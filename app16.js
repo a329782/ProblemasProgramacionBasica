bf753bd63e8e8eb60c151e0100be1878c6540398
a329782 <a329782@uach.mx>
2021-04-27 14:43:50 -0600

    16.- Imprimir diez veces la serie de números del 1 al 10.

---
+++ app16.js
@@ -0,0 +1,10 @@
+//16.- Imprimir diez veces la serie de números del 1 al 10.
+
+for(let i = 1; i < 11; i++)
+{
+    for(let j = 1; j < 11; j++)
+    {
+        process.stdout.write(j + " ");
+    }
+    console.log("");
+}
